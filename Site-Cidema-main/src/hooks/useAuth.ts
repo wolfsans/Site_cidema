9c90f20e5fa9e0db05590ba3362c784f495fad21
Luiz Augusto Lise <125785510+wolfsans@users.noreply.github.com>
2026-07-08 11:50:23 -0300

    Delete Site-Cidema-main directory

--- Site-Cidema-main/src/hooks/useAuth.ts
+++
@@ -1,8 +0,0 @@
-export const useAuth = () => {
-  return {
-    isAuthenticated: true, // Mocked to true for demonstration
-    user: { id: '1', name: 'Admin', email: 'admin@cidema.sc.gov.br', role: 'admin' },
-    login: () => {},
-    logout: () => {}
-  }
-};
